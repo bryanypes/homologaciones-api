bd3634d7c190f9445ba28afc35cd8ab507142117
Tu Nombre <tuemail@gmail.com>
2026-07-18 17:12:56 -0500

    Seed de usuarios

--- app/main.py
+++ app/main.py
@@ -4,13 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.v1 import auth, solicitudes, documentos, homologaciones, catalogos, academico, usuarios
 from app.core.database import AsyncSessionLocal
-from app.core.seed import seed_catalogos
+from app.core.seed import seed_catalogos, crear_usuario_inicial
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     async with AsyncSessionLocal() as db:
         await seed_catalogos(db)
+        await crear_usuario_inicial(db)
     yield
 
 
